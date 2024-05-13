62be4990d5e7ad73423cd6b17808903a39903cee
ElisabettaMirti <belmirti@gmail.com>
2024-05-13 17:00:42 +0200

    inserito script bonus

---
+++ APièDispari/js/script.js
@@ -0,0 +1,9 @@
+let numberList = [""];
+
+for (let i = 0; i < 6; i++ ) {
+    let userNumber = number.parseInt(prompt("Type a number"))
+
+    if (userNumber % 2 === 0) {
+        numberList.push(userNumber)
+    }
+}
